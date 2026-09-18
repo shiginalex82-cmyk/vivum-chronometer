a9a7dd7e3510bfb081f5499e397cefd55498ff73
shiginalex82-cmyk <shiginalex82@gmail.com>
2026-09-18 15:59:07 +0300

    Add offline service worker

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='vivum-chrono-v1',A=['./','./index.html','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html')))));
